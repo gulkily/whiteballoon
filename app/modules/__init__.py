ada17d67036be7f21f6cbffe06f0ba46588d0f09
Ilya Gulko <ilya@ilyagulko.com>
2025-10-22 19:20:28 -0400

    feat: toggle request form on demand

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -3,10 +3,12 @@
 from fastapi import FastAPI
 
 from app.modules.requests import router as requests_router
+from app.modules.requests import templates as requests_templates
 
 
 def register_modules(app: FastAPI) -> None:
     app.include_router(requests_router)
+    app.include_router(requests_templates.router)
 
 
 __all__ = ["register_modules"]
